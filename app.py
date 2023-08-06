fbce5c184ed506b62d2548624e3d7469a9e17f5f
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-07 00:53:03 +0300

    u

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import streamlit as st
+import utilities as utl
+
 st.title('Test')
+
+# Usage
+tickers = utl.scrape('https://www.argaam.com/en/company/companies-prices', ['2222'])
+
+st.dataframe(tickers)
---
+++ utilities.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import yfinance as yf
+from autoscraper import AutoScraper
+
+def scrape(url, wanted_list):
+    """Scrape and retrieve tickers from the provided URL."""
+    scraper = AutoScraper()
+    tickers = scraper.build(url, wanted_list)
+    tickers = {ticker.strip() + '.SR' for ticker in tickers}
+    return tickers
+
+# def svix(ticker, lookback, period, interval, buy_threshold):
+#     """Compute svix and buy signals for a given ticker."""
+#     ticker_data = yf.Ticker(ticker)
+#     df = ticker_data.history(period=period, interval=interval).ffill()[['High', 'Low', 'Close']]
+    
+#     df['HHigh'] = df['High'].rolling(lookback).max()
+#     df['LLow'] = df['Low'].rolling(lookback).min()
+#     df['svix'] = (1 - (df['Close'] - df['LLow']) / (df['HHigh'] - df['LLow'])) * 100
+#     df['buy'] = (df['svix'] > buy_threshold).astype(int)
+#     df['bought'] = df['buy'] * df['Close']
+    
+#     return df
