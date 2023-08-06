413756b0038469a82a2178b2711457b99772bd9c
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-07 00:55:20 +0300

    u

--- utilities.py
+++ utilities.py
@@ -7,17 +7,18 @@ def scrape(url, wanted_list):
     scraper = AutoScraper()
     tickers = scraper.build(url, wanted_list)
     tickers = {ticker.strip() + '.SR' for ticker in tickers}
+    tickers = pd.DataFrame(tickers)
     return tickers
 
-# def svix(ticker, lookback, period, interval, buy_threshold):
-#     """Compute svix and buy signals for a given ticker."""
-#     ticker_data = yf.Ticker(ticker)
-#     df = ticker_data.history(period=period, interval=interval).ffill()[['High', 'Low', 'Close']]
+def svix(ticker, lookback, period, interval, buy_threshold):
+    """Compute svix and buy signals for a given ticker."""
+    ticker_data = yf.Ticker(ticker)
+    df = ticker_data.history(period=period, interval=interval).ffill()[['High', 'Low', 'Close']]
     
-#     df['HHigh'] = df['High'].rolling(lookback).max()
-#     df['LLow'] = df['Low'].rolling(lookback).min()
-#     df['svix'] = (1 - (df['Close'] - df['LLow']) / (df['HHigh'] - df['LLow'])) * 100
-#     df['buy'] = (df['svix'] > buy_threshold).astype(int)
-#     df['bought'] = df['buy'] * df['Close']
+    df['HHigh'] = df['High'].rolling(lookback).max()
+    df['LLow'] = df['Low'].rolling(lookback).min()
+    df['svix'] = (1 - (df['Close'] - df['LLow']) / (df['HHigh'] - df['LLow'])) * 100
+    df['buy'] = (df['svix'] > buy_threshold).astype(int)
+    df['bought'] = df['buy'] * df['Close']
     
-#     return df
+    return df
