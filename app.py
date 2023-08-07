12aa0e6c5b77a4424134ea0e4c86614d5282a004
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-08 01:32:02 +0300

    u

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import utilities as utl
 
 st.title('Test')
 
+lookback = st.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
+
 # Usage
 tickers = utl.scrape('https://www.argaam.com/en/company/companies-prices', ['2222'])
 # st.write(tickers)
-df = utl.compute_svix_for_tickers(tickers)
+# df = utl.compute_svix_for_tickers(tickers)
 
 st.dataframe(df)
