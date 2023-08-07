9f6a4c4f0301d4837c8b077dc1178125aa09c429
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-08 02:33:55 +0300

    u

--- app.py
+++ app.py
@@ -9,15 +9,8 @@ st.title('Test')
 # lookback = st.sidebar.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
 
 col1, col2 = st.columns([1,3])
-
 interval = col1.selectbox('Interval', ['Daily', 'Weekly'])
 lookback = col2.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
-
-
-
-# Usage
 tickers = utl.scrape('https://www.argaam.com/en/company/companies-prices', ['2222'])
-# st.write(tickers)
-# df = utl.compute_svix_for_tickers(tickers)
-
-st.dataframe(df)
+result = utl.get_metric(tickers, interval, lookback)
+st.dataframe(result)
--- utilities.py
+++ utilities.py
@@ -47,3 +47,50 @@ def compute_svix_for_tickers(tickers):
         
     return pd.DataFrame(data, columns=['ticker', 'name', 'svix'])
 
+
+
+#########
+
+import yfinance as yf
+from datetime import datetime, timedelta
+
+def compute_start_date(interval: str, lookback: int) -> datetime:
+    today = datetime.today()
+    
+    if interval == "Daily":
+        # Convert business days to calendar days (excluding Fridays and Saturdays)
+        calendar_days = lookback + 2 * (lookback // 5)
+        start_date = today - timedelta(days=calendar_days)
+    elif interval == "Weekly":
+        # Calculate how many days to go back to the previous Sunday
+        days_to_last_sunday = today.weekday() + 1
+        start_date = today - timedelta(days=days_to_last_sunday + (lookback-1)*7)
+    else:
+        raise ValueError("Invalid interval. Please choose 'Daily' or 'Weekly'.")
+    
+    return start_date
+
+def get_metric(tickers, interval, lookback):
+    start_date = compute_start_date(interval, lookback)
+    
+    metrics = {}
+    
+    for ticker in tickers:
+        # Fetch the daily data
+        data = yf.download(ticker, interval="1d", start=start_date.strftime('%Y-%m-%d'), progress=False)
+        
+        if interval == "Weekly":
+            # Filter only the Thursdays' data
+            data = data[data.index.weekday == 3]
+        
+        # Extract required data
+        latest_close = data['Close'].iloc[-1]
+        lowest_low = data['Low'].min()
+        highest_high = data['High'].max()
+        
+        # Compute the metric
+        metric = 1 - (latest_close - lowest_low) / (highest_high - lowest_low)
+        metrics[ticker] = metric
+
+    return metrics
+
