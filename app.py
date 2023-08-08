38435c6e4f621a6256ec7f93f5fba16883a0d1d3
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-08 04:01:57 +0300

    u

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import utilities as utl
 
 st.title('Test')
 
+tickers = utl.scrape('https://www.argaam.com/en/company/companies-prices', ['2222'])
+data_dict = utl.download_data(tickers)
+
+df = pd.DataFrame(data_dict).T
+
 col1, col2 = st.columns([1,3])
 interval = col1.selectbox('Interval', ['Daily', 'Weekly'])
 lookback = col2.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
+marketcap = st.slider('Market Cap', min_value=df['marketCap'].min(), max_value=df['marketCap'].max())
 
 # # Sample usage:
 
@@ -21,8 +27,6 @@ lookback = col2.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
 # result = compute_metric_from_data(data_dict, interval, lookback)
 # print(result)
 
-tickers = utl.scrape('https://www.argaam.com/en/company/companies-prices', ['2222'])
-data_dict = utl.download_data(tickers)
 
 result = utl.compute_metric_from_data(data_dict, interval, lookback)
 st.dataframe(pd.DataFrame(result).T)
