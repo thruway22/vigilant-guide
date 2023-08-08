5de152d504b86a311ba61958202292a6d3d3f523
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-09 01:15:19 +0300

    u

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ lookback = col2.slider('Lookback', min_value=3, max_value=52, value=20, step=1)
 
 result = compute_metric_from_data(data_dict, interval, lookback)
 result = create_dataframe(result)
-st.dataframe(result)
+st.dataframe(result, use_container_width=True)
 
