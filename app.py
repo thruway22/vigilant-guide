dd4202867cf3c4302b097d180401470c5faec02e
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-08 03:42:31 +0300

    u

--- app.py
+++ app.py
@@ -8,9 +8,16 @@ st.title('Test')
 # interval = st.sidebar.selectbox('Interval', ['Daily', 'Weekly'])
 # lookback = st.sidebar.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
 
-col1, col2 = st.columns([1,3])
-interval = col1.selectbox('Interval', ['Daily', 'Weekly'])
-lookback = col2.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
-tickers = utl.scrape('https://www.argaam.com/en/company/companies-prices', ['2222'])
-result = utl.get_metric(tickers, interval, lookback)
-st.dataframe(result)
+# col1, col2 = st.columns([1,3])
+# interval = col1.selectbox('Interval', ['Daily', 'Weekly'])
+# lookback = col2.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
+
+form = st.sidebar.form("form")
+interval = st.sidebar.selectbox('Interval', ['Daily', 'Weekly'])
+lookback = st.sidebar.slider('Lookback', min_value=1, max_value=52, value=20, step=1)
+submitted = st.sidebar.form.form_submit_button("Show Results")
+
+
+# tickers = utl.scrape('https://www.argaam.com/en/company/companies-prices', ['2222'])
+# result = utl.get_metric(tickers, interval, lookback)
+# st.dataframe(result)
